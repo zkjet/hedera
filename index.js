15e74415b5cc43896ceec9a133f75bb8d826f279
zkjet <zkjet@mailfence.com>
2022-07-05 16:59:42 -0600

    setup account id and priv key

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client } = require("@hashgraph/sdk");
+require("dotenv").config();
+
+async function main() {
+  // Grab your Hedera testnet account ID and private key from your .env file
+  const myAccountId = process.env.MY_ACCOUNT_ID;
+  const myPrivateKey = process.env.MY_PRIVATE_KEY;
+
+  // If we weren't able to grab it, we should throw a new error
+  if (myAccountId == null || myPrivateKey == null) {
+    throw new Error(
+      "Environment variables myAccountId and myPrivateKey must be present"
+    );
+  }
+}
+main();
