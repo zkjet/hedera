c10bc1905c45d08626cff442f3de586d51b5d884
zkjet <zkjet@mailfence.com>
2022-07-05 17:42:18 -0600

    create topic, grab topic id, wait 5 seconds for consensus

--- index.js
+++ index.js
@@ -9,52 +9,25 @@ const {
   TopicMessageSubmitTransaction,
 } = require("@hashgraph/sdk");
 
+// Grab the OPERATOR_ID and OPERATOR_KEY from the .env file
+const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
+const operatorkey = PrivateKey.fromString(process.env.OPERATOR_KEY);
+
+// BUILD HEDERA testnet and mirror node client
+const client = Client.forTestnet();
+
+// Set the operator account ID and operator private key
+client.setOperator(operatorId, operatorKey);
+
 async function main() {
-  // Grab your Hedera testnet account ID and private key from your .env file
-  const myAccountId = process.env.MY_ACCOUNT_ID;
-  const myPrivateKey = process.env.MY_PRIVATE_KEY;
+  //Create a new Topic
+  let txResponse = await new TopicCreateTransaction().execute(client);
+  // Grab the newly generated Topic ID
+  let receipt = await txResponse.getReceipt(client);
+  let topicId = receipt.topicId;
+  console.log(`Your Topic ID is: ${topicId}`);
 
-  // If we weren't able to grab it, we should throw a new error
-  if (myAccountId == null || myPrivateKey == null) {
-    throw new Error(
-      "Environment variables myAccountId and myPrivateKey must be present"
-    );
-  }
-  // Create our connection to the Hedera network
-  // The Hedera JS SDK Makes this really easy!
-  const client = Client.forTestnet();
-
-  client.setOperator(myAccountId, myPrivateKey);
-
-  // const client = Client.forTestnet();
-  //client.setOperator(myAccountId, myPrivateKey);
-
-  //-----------------------<enter code below>--------------------------------------
-
-  //Create new Keys
-  const newAccountPrivateKey = await PrivateKey.generateED25519();
-  const newAccountPublicKey = newAccountPrivateKey.publicKey;
-
-  //Create a new account with 1,000 tinybar starting balance
-  const newAccount = await new AccountCreateTransaction()
-    .setKey(newAccountPublicKey)
-    .setInitialBalance(Hbar.fromTinybars(1000))
-    .execute(client);
-  // Get the new account ID
-  const getReceipt = await newAccount.getReceipt(client);
-  const newAccountId = getReceipt.accountId;
-
-  console.log("The new account ID is: " + newAccountId);
-
-  // Verify the account balance
-  const accountBalance = await new AccountBalanceQuery()
-    .setAccountId(newAccountId)
-    .execute(client);
-
-  console.log(
-    "Thew new account balance is: " +
-      accountBalance.hbars.toTinybars() +
-      " tinybar."
-  );
+  // Wait 5 seconds between consensus topic creation and subscription creation
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 }
 main();
