e2ea26818c166748bfdaa9873b965aae768a3461
zkjet <zkjet@mailfence.com>
2022-07-05 17:06:08 -0600

    generate keys for new account

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const { Client } = require("@hashgraph/sdk");
+const {
+  Client,
+  PrivateKey,
+  AccountCreateTransaction,
+  AccountBalanceQuery,
+  Hbar,
+} = require("@hashgraph/sdk");
 require("dotenv").config();
+// const client = Client.forTestnet();
+//client.setOperator(myAccountId, myPrivateKey);
+
+//-----------------------<enter code below>--------------------------------------
+
+//Create new Keys
+const newAccountPrivateKey = await PrivateKey.generateED25519();
+const newAccountPublicKey = newAccountPrivateKey.publicKey;
 
 async function main() {
   // Grab your Hedera testnet account ID and private key from your .env file
@@ -12,5 +26,10 @@ async function main() {
       "Environment variables myAccountId and myPrivateKey must be present"
     );
   }
+  // Create our connection to the Hedera network
+  // The Hedera JS SDK Makes this really easy!
+  const client = Client.forTestnet();
+
+  client.setOperator(myAccountId, myPrivateKey);
 }
 main();
