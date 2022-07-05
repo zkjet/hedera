901d5db60a50be27669b3496f2ec320efe324f5b
zkjet <zkjet@mailfence.com>
2022-07-05 17:47:36 -0600

    get status of transaction

--- index.js
+++ index.js
@@ -39,12 +39,15 @@ async function main() {
         `${message.consensusTimestamp.toDate()} Received: ${messageAsString}`
       );
     });
-    //Send one message
-    let sendResponse = await new TopicMessageSubmitTransaction({
-        topicid: topicId,
-        message: "Hello, HCS!",
-    }).execute(client);
-    const getReceipt = await sendResponse.getReceipt(client)
-    
+  //Send one message
+  let sendResponse = await new TopicMessageSubmitTransaction({
+    topicid: topicId,
+    message: "Hello, HCS!",
+  }).execute(client);
+  const getReceipt = await sendResponse.getReceipt(client);
+
+  // Get the status of the transaction
+  const transactionStatus = getReceipt.status;
+  console.log("The message transaction status" + transactionStatus);
 }
 main();
