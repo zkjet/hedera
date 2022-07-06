7425bf3309d336361d21ef11f44f0020f99103ba
zkjet <zkjet@mailfence.com>
2022-07-05 18:33:22 -0600

    send message succesfully

--- index.js
+++ index.js
@@ -9,28 +9,42 @@ const {
   TopicMessageSubmitTransaction,
 } = require("@hashgraph/sdk");
 
-// Grab the OPERATOR_ID and OPERATOR_KEY from the .env file
-const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
-const operatorkey = PrivateKey.fromString(process.env.OPERATOR_KEY);
-
-// BUILD HEDERA testnet and mirror node client
-const client = Client.forTestnet();
+// // Grab the OPERATOR_ID and OPERATOR_KEY from the .env file
+// const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
+// const operatorKey = PrivateKey.fromString(process.env.OPERATOR_KEY);
 
 // Set the operator account ID and operator private key
-client.setOperator(operatorId, operatorKey);
+// client.setOperator(operatorId, operatorKey);
 
 async function main() {
-  //Create a new Topic
+  //Grab your Hedera testnet account ID and private key from your .env file
+  const myAccountId = process.env.MY_ACCOUNT_ID;
+  const myPrivateKey = process.env.MY_PRIVATE_KEY;
+
+  // If we weren't able to grab it, we should throw a new error
+  if (myAccountId == null || myPrivateKey == null) {
+    throw new Error(
+      "Environment variables myAccountId and myPrivateKey must be present"
+    );
+  }
+
+  // Build Hedera testnet and mirror node client
+  const client = Client.forTestnet();
+
+  client.setOperator(myAccountId, myPrivateKey);
+
+  //Create a new topic
   let txResponse = await new TopicCreateTransaction().execute(client);
-  // Grab the newly generated Topic ID
+
+  //Grab the newly generated topic ID
   let receipt = await txResponse.getReceipt(client);
   let topicId = receipt.topicId;
-  console.log(`Your Topic ID is: ${topicId}`);
+  console.log(`Your topic ID is: ${topicId}`);
 
   // Wait 5 seconds between consensus topic creation and subscription creation
   await new Promise((resolve) => setTimeout(resolve, 5000));
 
-  // Create the query
+  //Create the query
   new TopicMessageQuery()
     .setTopicId(topicId)
     .subscribe(client, null, (message) => {
@@ -39,14 +53,15 @@ async function main() {
         `${message.consensusTimestamp.toDate()} Received: ${messageAsString}`
       );
     });
-  //Send one message
+
+  // Send one message
   let sendResponse = await new TopicMessageSubmitTransaction({
-    topicid: topicId,
+    topicId: topicId,
     message: "Hello, HCS!",
   }).execute(client);
   const getReceipt = await sendResponse.getReceipt(client);
 
-  // Get the status of the transaction
+  //Get the status of the transaction
   const transactionStatus = getReceipt.status;
   console.log("The message transaction status" + transactionStatus);
 }
